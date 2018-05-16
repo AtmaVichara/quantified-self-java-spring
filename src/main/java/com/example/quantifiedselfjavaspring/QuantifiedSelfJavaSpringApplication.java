6335dfdd12844d79fd4cb934355d5827f858343b
Joe Jobes <jmrjobes@gmail.com>
2018-05-15 18:18:31 -0600

    Add initial environment setup and gradle build
    * Add postgres, jpa, junit, hateoas, and mockito to gradle.build
    * Compile project
    * Boot run project, building production files

---
+++ src/main/java/com/example/quantifiedselfjavaspring/QuantifiedSelfJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.quantifiedselfjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantifiedSelfJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantifiedSelfJavaSpringApplication.class, args);
+	}
+}
